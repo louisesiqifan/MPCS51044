bbd6ab260498efd20ace59c568a32ed7b4655545
Louise Fan <louisesiqifan@gmail.com>
2021-01-24 10:34:09 -0600

    part 2 done

--- HW2/HW2_1.cpp
+++ HW2/HW2_1.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 #include <algorithm>
 #include <numeric>
+#include <cmath>
 using namespace std;
 
 int main () {
     vector<double> v = {1.1, 1.2, 1.3, 1.4};
     vector<double> w;
 
-    cout << "Original Vector: "
+    cout << "Original Vector: ";
     for (auto i:v) {
         cout << i << " ";
     }
@@ -16,10 +17,15 @@ int main () {
 
     transform(v.begin(), v.end(), back_inserter(w), [](const auto n) { return n*n;});
 
-    cout << "New Vector: "
+    cout << "New Vector: ";
     for (auto i:w) {
         cout << i << " ";
     }
     cout << "\n";
+
+    cout << "Distance to origin by accumulate and sqrt: ";
+    double d = sqrt(accumulate(w.begin(), w.end(), 0.0));
+    cout << d << "\n";
+
     return 0;
 }
