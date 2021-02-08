634584ba4bebc50aa3a5ee4846742cde47149ff0
Louise Fan <louisesiqifan@gmail.com>
2021-02-08 13:09:38 -0600

    HW4_1 + done

---
+++ HW4/HW4_1/Matrix.cpp
@@ -0,0 +1,17 @@
+#include "Matrix.h"
+#include <iostream>
+#include <chrono>
+using namespace mpcs51044;
+using namespace std;
+
+int main()
+{
+	Matrix<3, 3> m = {
+			{ 1, 2, 3, },
+			{ 4, 5, 6, },
+			{ 7, 8, 9, }
+	};
+	Matrix<3, 3> result = m + m;
+	cout << result;
+	return 0;
+}
